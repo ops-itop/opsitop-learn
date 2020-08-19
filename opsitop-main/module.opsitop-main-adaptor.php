cafa1b1a16dab546f53a887504f2b5a1a0353264
annProg <tecbbs@qq.com>
2020-08-19 12:55:45 +0800

    test delete details presentation

--- opsitop-main/module.opsitop-main-adaptor.php
+++ opsitop-main/module.opsitop-main-adaptor.php
@@ -17,7 +17,7 @@ SetupWebPage::AddModule(
 		//
 		'dependencies' => array(
 			'itop-config-mgmt/2.7.0',
-			'teemip-ip-mgmt/2.6.1'
+			'teemip-config-mgmt-adaptor/2.6.1'
 		),
 		'mandatory' => false,
 		'visible' => true, // To prevent auto-install but shall not be listed in the install wizard
